52abdf2b73ad4e304d76817365c19a2216c94b33
Tareq-Adnan <adnantareq2@gmail.com>
2026-04-09 16:31:27 +0600

    version updater

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026040900;           // YYYYMMDDXX.
+$plugin->version   = 2026040910;           // YYYYMMDDXX.
 $plugin->release   = '1.0.0';
 $plugin->requires  = 2022041900;           // Moodle 4.0+.
 $plugin->maturity  = MATURITY_STABLE;
